3e85de8dfc8a425fe65ce09add231a4d4e5e2c3f
regokan <regokan@gmail.com>
2024-10-21 09:57:13 +0530

    chore: update gitignore, config, and main files to support IAM, Lambda, and Keyspaces modules

--- infra/config.tf
+++ infra/config.tf
@@ -10,7 +10,7 @@ terraform {
 
 # Configure the AWS provider
 provider "aws" {
-  region = var.region
+  region     = var.region
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
--- infra/main.tf
+++ infra/main.tf
@@ -1,11 +1,32 @@
 terraform {
   backend "s3" {
     bucket = "sparkify-etl-tf-state"
-    key = "terraform.tfstate"
+    key    = "terraform.tfstate"
     region = "us-east-1"
   }
 }
 
 module "s3" {
   source = "./modules/s3"
+}
+
+module "iam" {
+  source = "./modules/iam"
+
+  aws_region              = data.aws_region.current.name
+  aws_account_id          = data.aws_caller_identity.current.account_id
+  sparkify_etl_bucket_arn = module.s3.sparkify_etl_bucket_arn
+}
+
+module "keyspaces" {
+  source = "./modules/keyspaces"
+}
+
+module "lambda" {
+  source = "./modules/lambda"
+
+  sparkify_etl_bucket_id       = module.s3.sparkify_etl_bucket_id
+  sparkify_etl_bucket_arn      = module.s3.sparkify_etl_bucket_arn
+  sparkify_bronze_etl_role_arn = module.iam.sparkify_bronze_etl_role_arn
+
 }
